7a842dbbee0fc9be4d06cf5cabe17544b2998db1
div_250 <inezadivine41@example.com>
2025-09-17 03:29:52 -0700

    six

--- src/Atm.java
+++ src/Atm.java
@@ -1,13 +1,15 @@
 public class Atm {
     public void deposit(int amount) {
+        System.out.println("Deposited $" + amount + " in cash");
+    }
 
 
-        public void deposit (String checkNumber,int amount){
-            System.out.println("Deposited $" + amount + " using check #" + checkNumber);
-        }
+    public void deposit(String checkNumber, int amount) {
+        System.out.println("Deposited $" + amount + " using check #" + checkNumber);
+    }
 
-        public void deposit (String phoneNumber,double amount){
-            System.out.println("Deposited $" + amount + " via mobile transfer from " + phoneNumber);
-        }
+
+    public void deposit(String phoneNumber, double amount) {
+        System.out.println("Deposited $" + amount + " via mobile transfer from " + phoneNumber);
     }
 }
